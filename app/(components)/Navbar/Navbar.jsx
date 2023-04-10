bf01fea2f3b44a030a3063b63eb22d5776e4817a
Vaalgtir <e.eniona2@gmail.com>
2023-04-10 23:49:36 +0200

    init

---
+++ app/(components)/Navbar/Navbar.jsx
@@ -0,0 +1,40 @@
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import "./Navbar.scss";
+
+import logo from "./../../../assets/logo.svg";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Navbar() {
+  const links = [
+    "Modèles",
+    "Achat et Location",
+    "Electrique et hybride",
+    "Entretien et Service",
+    "Notre Marque",
+  ];
+
+  return (
+    <div className="wrapperNav">
+      <div className="wrapperActions">
+        <div className="wrapperLinks">
+          {links.map((item, key) => (
+            <div key={key} className="item">
+              {item}
+            </div>
+          ))}
+
+          <Image
+            src={logo}
+            alt="logo"
+            className="logo"
+            sizes="(max-width: 768px) 100vw,
+                  (max-width: 1200px) 100vw,
+                  100vw"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
